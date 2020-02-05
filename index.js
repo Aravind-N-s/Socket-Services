0c18f6430c3f7f4c76f478848ae5f5b02eec53f3
Asura <aravind1808@gmail.com>
2020-02-05 09:09:03 +0530

    Removed Hosting

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const router = require('./Config/routes')
 const http = require('http').createServer(app)
 const io = require('socket.io').listen(http)
 
-//for heroku 
-const path = require("path")
-const port = process.env.PORT
 
 http.listen(3001, function(){
   console.log('listening on *:3001')
@@ -32,12 +29,6 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/chat', router)
-
-app.use(express.static(path.join(__dirname,"client/build")))
-
-app.get("*", (req,res) =>{
-    res.sendFile(path.join(__dirname + "/client/build/index.html"))
-})
 
 app.listen(port ,() =>{
     console.log('Listening on port', port)
